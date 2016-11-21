89996071ede7f62b128b9a0dcdf249b5c82740f3
Filipe Scaglia <raijuuin@users.noreply.github.com>
2016-11-21 18:47:49 -0200

    Update dowhile5.c
    Estrutura for para do while

--- Aula 09/dowhile5.c
+++ Aula 09/dowhile5.c
@@ -2,14 +2,14 @@
 #include<stdlib.h>
 
 void main(){
-	int num, fat, i;
-	num = 6;
+	int fat=6, i=5;
 	
 	do{
-		for(fat = 1; num > 1; num = num -1)
-		fat = fat * num;
-		printf("Fatorial: %d", fat);
+		fat = fat * i;
+		printf("\nFatorial de 6: %d", fat);
+		i--;
 	}
-	while(fat<=num);
+	while(i>1);
+	
 	getch();
 }
